da2049d6fec42e9b6a34c290f67aad2d9a1df3bd
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-08-17 03:27:46 +0700

    Connect MongoDB

--- index.ts
+++ index.ts
@@ -1,14 +1,22 @@
 import express , { Express , Request , Response } from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+import Article from "./models/article.model";
 
 const app : Express  = express()
 const port : number | string = process.env.PORT || 3001;
 
-
+dotenv.config();
+database.connect();
 
 // Rest API
-app.get('/articles',(req : Request, res : Response) => {
+app.get('/articles', async(req : Request, res : Response) => {
+    const articles = await Article.find({
+        deleted : false
+    })
+    
     res.json({
-        articles : []
+        articles : articles
     })
 })
 
