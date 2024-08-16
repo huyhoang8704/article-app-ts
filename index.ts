a55a3828ca3e0a4fa5cf457f2bf7760aa016fe3b
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-08-17 03:44:17 +0700

    GraphQL Cơ bản

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import express , { Express , Request , Response } from "express";
 import dotenv from "dotenv";
 import * as database from "./config/database";
+
+
+
 import Article from "./models/article.model";
+import { ApolloServer, gql } from "apollo-server-express";
 
 const app : Express  = express()
 const port : number | string = process.env.PORT || 3001;
@@ -9,18 +13,37 @@ const port : number | string = process.env.PORT || 3001;
 dotenv.config();
 database.connect();
 
-// Rest API
-app.get('/articles', async(req : Request, res : Response) => {
-    const articles = await Article.find({
-        deleted : false
+const startServer = async () => {
+    // GraphQL
+    // Như Models
+    const typeDefs = gql`
+        type Query {
+        hello : String
+        }
+    `;
+    // Như Controllers
+    const resolvers = {
+        Query : {
+            hello : () => "Hello World"
+        }
+    }
+    const apollpServer = new ApolloServer({
+        typeDefs,
+        resolvers
     })
-    
-    res.json({
-        articles : articles
+    await apollpServer.start();
+        apollpServer.applyMiddleware({
+        app : app,
+        path : '/graphql'
     })
-})
+
+    app.listen(port , () =>{
+        console.log(`App listening on port ${port}`);
+    })
+}
+
+//! "hello": "Hello World" (key : value)
+
+startServer();
 
 
-app.listen(port , () =>{
-    console.log(`App listening on port ${port}`);
-})
