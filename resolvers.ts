3f3a61f020d475ae7982733617127867618ba2e7
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-08-17 04:20:27 +0700

    Create : Thêm mới 1 bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,3 +1,4 @@
+import { Mutation } from './node_modules/apollo-server-core/src/plugin/schemaReporting/generated/operations.d';
 import Article from "./models/article.model"
 
 export const resolvers = {
@@ -20,21 +21,46 @@ export const resolvers = {
             return article;
         }
         
+    },
+    Mutation : {
+        createArticle : async (_ , args) =>{
+            const {title, avatar, description} = args;
+            const article = new Article({
+                title : title,
+                avatar : avatar,
+                description : description
+            })
+            await article.save();
+            return article 
+        }
     }
 }
 
 
 /**
+ * Get : Lấy ra nhiều bài viết
     query {
         getListArticles{
             title,
             avatar
         }
     }
+ * Get : Lấy ra 1 bài viết
     query {
         getArticle(id : "6545d26fd3edd487ec1691b4"){
             title,
             avatar
+        }
+    }
+ * Create : Thêm mới 1 bài viết
+    mutation {
+        createArticle(
+            title :  "Bai bao 1",
+            avatar : "Link", 
+            description : "Mo ta",
+        ) {
+            id
+            title
         }
     }
  */
--- typeDefs.ts
+++ typeDefs.ts
@@ -1,15 +1,25 @@
+import { Mutation } from './node_modules/apollo-server-core/src/plugin/schemaReporting/generated/operations.d';
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
-    type Query {
-        hello : String,
-        getArticle(id : ID) : Article,
-        getListArticles : [Article]
-    }
     type Article {
         id : ID,
         title : String,
         avatar : String,
         description: String,
     }
+
+    type Query {
+        hello : String,
+        getArticle(id : ID) : Article,
+        getListArticles : [Article]
+    }
+    type Mutation {
+        createArticle(
+            title : String, 
+            avatar : String, 
+            description : String
+        ) : Article
+    }
+
 `;
